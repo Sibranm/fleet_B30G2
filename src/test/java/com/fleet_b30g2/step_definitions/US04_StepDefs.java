ec9446a06919113f0f21086dbac46e2930ae2155
mahinur <mahinur301@hotmail.com>
2023-11-01 12:37:18 +0100

    US04 updated

--- src/test/java/com/fleet_b30g2/step_definitions/US04_StepDefs.java
+++ src/test/java/com/fleet_b30g2/step_definitions/US04_StepDefs.java
@@ -1,5 +1,6 @@
 package com.fleet_b30g2.step_definitions;
 
+import com.fleet_b30g2.pages.BasePage;
 import com.fleet_b30g2.pages.LoginPage;
 import com.fleet_b30g2.pages.VehicleContractsPage;
 import com.fleet_b30g2.utilities.BrowserUtils;
@@ -11,6 +12,7 @@ import io.cucumber.java.en.When;
 import org.apache.commons.collections.FactoryUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -22,5 +24,72 @@ import java.util.List;
 public class US04_StepDefs {
 
     VehicleContractsPage vehicleContractsPage = new VehicleContractsPage();
+
+    @When("Store manager click the Fleet button")
+    public void storeManagerClickTheFleetButton() {
+        vehicleContractsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.hover(vehicleContractsPage.fleetDropdown);
+    }
+
+    @When("Store manager click the Vehicle Contract button")
+    public void store_manager_click_the_vehicle_contract_button() {
+        BrowserUtils.sleep(2);
+        vehicleContractsPage.vehicleContracts.click();
+
+    }
+    @Then("Store manager see title is as expected")
+    public void store_manager_see_title_is_as_expected() {
+        BrowserUtils.sleep(2);
+        String expectedTitle = "All - Vehicle Contract - Entities - System - Car - Entities - System";//bug
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+    }
+
+    @When("Sales manager the Fleet button")
+    public void salesManagerTheFleetButton() {
+        vehicleContractsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.hover(vehicleContractsPage.fleetDropdown);
+    }
+
+
+    @When("Sales manager click the Vehicle Contract button")
+    public void sales_manager_click_the_vehicle_contract_button() {
+        BrowserUtils.sleep(2);
+        vehicleContractsPage.vehicleContracts.click();
+
+    }
+    @Then("Sales manager see title is as expected")
+    public void sales_manager_see_title_is_as_expected() {
+        BrowserUtils.sleep(2);
+        String expectedTitle = "All - Vehicle Contract - Entities - System - Car - Entities - System"; //bug
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+    }
+
+    @When("Driver click the Fleet button")
+    public void driverClickTheFleetButton() {
+        vehicleContractsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.hover(vehicleContractsPage.fleetDropdown);
+    }
+
+    @When("Driver click the Vehicle Contract button")
+    public void driver_click_the_vehicle_contract_button() {
+        BrowserUtils.sleep(2);
+        vehicleContractsPage.driverFleet.click();
+
+    }
+    @Then("App should display no permission message")
+    public void app_should_display_no_permission_message() {
+        BrowserUtils.verifyElementDisplayed(vehicleContractsPage.permissionMessage);
+
+    }
+
+
+
+
+
+
 
 }
--- src/test/resources/features/US04_VehileContracts.feature
+++ src/test/resources/features/US04_VehileContracts.feature
@@ -3,24 +3,32 @@ Feature: User should be able to access to Vehicle contracts page
   Agile story: As a user, I want to access to Vehicle contracts page
 
   Background: For all the scenarios user is on the log in page.
+    Given the user is on the login page
 
   Scenario: Menu option for the Store managers
-    Given user is on the home page
-    When the user hover over the mouse on the Fleet button
-    And the user click the Vehicle Contract button
-    Then the user should see "All - Vehicle Contract - Entities - System - Car -Entities - System" in the Title
+    Given the user logged in as "Store manager"
+    When Store manager click the Fleet button
+    And Store manager click the Vehicle Contract button
+    Then Store manager see title is as expected
+  #expectedTitle: All - Vehicle Contract - Entities - System - Car -Entities - System
+  #actualTitle: Vehicle Contract - Entities - System - Car -Entities - System
 
   Scenario: Menu option for the Sales managers
-    Given user is on the home page
-    When the user hover over the mouse on the Fleet button
-    And the user click the Vehicle Contract button
-    Then the user should see "All - Vehicle Contract - Entities - System - Car -Entities - System" in the Title
+    Given the user logged in as "Sales manager"
+    When Sales manager the Fleet button
+    And Sales manager click the Vehicle Contract button
+    Then Sales manager see title is as expected
+  #expectedTitle: All - Vehicle Contract - Entities - System - Car -Entities - System
+  #actualTitle: Vehicle Contract - Entities - System - Car -Entities - System
 
   Scenario: No Permission message for the Drivers
-    Given user is on the home page
-    When the user hover over the mouse on the Fleet button
-    And the user click the Vehicle Contract button
-    Then App should display "You do not have permission to perform this action."
+    Given the user logged in as "Driver"
+    When Driver click the Fleet button
+    And Driver click the Vehicle Contract button
+    Then App should display no permission message
+  #expectedMessage: You do not have permission to perform this action.
+  #actualMessage: You do not have permission to perform this action.
+
 
 
 
